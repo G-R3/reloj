f9f37b8cdba763a222b5e4c1dc714e4d8712ac57
G-R3 <gr261212@gmail.com>
2026-03-11 18:14:59 -0400

    finite state machine

--- lcd.ino
+++ lcd.ino
@@ -1,5 +1,10 @@
 #include <LiquidCrystal.h>
 
+struct FormattedTime {
+  int seconds;
+  int minutes;
+};
+
 const int pauseBtnPin = 6;
 
 const int rs = 12;
@@ -14,8 +19,8 @@ LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 unsigned long startTime;
 unsigned long passedTime;
 // const long focusTime = 1500000 // 25 minutes
-const long focusTime = 5000;  // 5 seconds
-const long breakTime = 4000;  // 4 seconds;
+const long focusTime = 10000;  // 10 seconds
+const long breakTime = 8000;   // 8 seconds;
 
 long remainingTime;
 
@@ -23,17 +28,25 @@ bool focusMode = true;
 
 int currentPauseState = 0;
 int prevPauseState = 0;
-int timerRunning = 1;
 
 unsigned long timerPausedAt;
 
+enum TimerState {
+  PAUSE,
+  RESUME,
+  RUNNING,
+};
+
+TimerState timerState = RUNNING;
+
 void setup() {
   lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
 
   startTime = millis();
 
   Serial.begin(9600);
-
   pinMode(pauseBtnPin, INPUT);
 }
 
@@ -56,7 +69,7 @@ void render(int minutes, int seconds, bool focusMode) {
 
 
   lcd.setCursor(5, 1);
-  if (!timerRunning) {
+  if (timerState == PAUSE) {
     lcd.print("PAUSED");
   } else {
     lcd.print("      ");
@@ -64,51 +77,74 @@ void render(int minutes, int seconds, bool focusMode) {
 }
 
 void toggleTimer(int currentPauseState) {
-  Serial.println("===== Attempting to toggle timer =====");
-  Serial.print("Timer running: ");
-  Serial.print(timerRunning);
-  Serial.print(" Pause state: ");
-  Serial.println(currentPauseState);
-
   if (currentPauseState == 1 && prevPauseState == 0) {
-    timerPausedAt = millis();
-    timerRunning = !timerRunning;
+    if (timerState == RUNNING) {
+      timerPausedAt = millis();
+      timerState = PAUSE;
+    } else if (timerState == PAUSE) {
+      timerState = RESUME;
+    }
   }
 }
 
 long getRemainingTime() {
   long remainingTime;
+  unsigned long elapsedTime = millis() - startTime;
 
   if (focusMode) {
-    remainingTime = focusTime - (millis() - startTime);
+    remainingTime = focusTime - elapsedTime;
   } else {
-    remainingTime = breakTime - (millis() - startTime);
+    remainingTime = breakTime - elapsedTime;
   }
 
   return remainingTime;
 }
 
-void loop() {
-  currentPauseState = digitalRead(pauseBtnPin);
-  toggleTimer(currentPauseState);
-
-  if (timerRunning) {
-    startTime = startTime + (millis() - timerPausedAt);
-
-    remainingTime = getRemainingTime();
-
-    if (remainingTime <= 0) {
-      remainingTime = 0;
-      startTime = millis();
-      focusMode = !focusMode;
-    }
-  }
+FormattedTime formatTime() {
+  FormattedTime t;
 
   long totalSeconds = (remainingTime) / 1000;
-  int seconds = totalSeconds % 60;
-  int minutes = totalSeconds / 60;
+  t.seconds = totalSeconds % 60;
+  t.minutes = totalSeconds / 60;
 
-  render(minutes, seconds, focusMode);
+  return t;
+}
+
+void loop() {
+  currentPauseState = digitalRead(pauseBtnPin);
+  Serial.println(currentPauseState);
+  toggleTimer(currentPauseState);
+
+  switch (timerState) {
+    case PAUSE:
+      {
+        auto time = formatTime();
+
+        render(time.minutes, time.seconds, focusMode);
+        break;
+      }
+    case RESUME:
+      {
+        startTime += millis() - timerPausedAt;
+        timerState = RUNNING;
+        break;
+      }
+    case RUNNING:
+      {
+        remainingTime = getRemainingTime();
+
+        if (remainingTime <= 0) {
+          remainingTime = 0;
+          startTime = millis();
+          focusMode = !focusMode;
+        }
+
+        auto time = formatTime();
+
+        render(time.minutes, time.seconds, focusMode);
+        break;
+      }
+  }
 
   prevPauseState = currentPauseState;
 }
@@ -134,6 +170,18 @@ void loop() {
     - push button to pause
     - push button to reset timer
     - push button to exit to menu.
+
+
+    Time states:
+      PAUSED:
+        - timer does not count down
+        - record the time at the moment of pausing
+      RESUME:
+        - timer updates startTime. startTime + (millis() - timePausedAt)
+          - millis() does not stop counting when we pause.
+      RUNNING:
+        - timer counts down
+        - do no recalculate startTime.
 
 
 */
