4ae27fc281a6e1f2926bc061d791922d39b20e0a
G-R3 <gr261212@gmail.com>
2026-03-30 17:54:51 -0400

    testing

--- lcd.ino
+++ lcd.ino
@@ -193,3 +193,25 @@ void loop() {
     }
   }
 }
+
+// #include "LiquidCrystal.h"
+// #include "app.h"
+
+// const int rs = 12;
+// const int enable = 11;
+// const int d4 = 5;
+// const int d5 = 4;
+// const int d6 = 3;
+// const int d7 = 2;
+
+
+// LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
+// App app(lcd);
+
+// void setup() {
+//   app.begin(millis());
+// }
+
+// void loop() {
+//   app.update();
+// }
