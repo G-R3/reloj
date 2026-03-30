6356b8756f600e1df4c1cf744ca54aa2fcb9cc3b
G-R3 <gr261212@gmail.com>
2026-03-30 17:53:31 -0400

    add lcd member and uncomment other render methods

--- display.cpp
+++ display.cpp
@@ -1,6 +1,7 @@
-#include "display.h";
+#include "display.h"
 
-Display::Display(LiquidCrystal& dp) : lcd(dp) {};
+Display::Display(LiquidCrystal& dp)
+  : lcd(dp) {}
 
 // pass object by reference
 void Display::begin() {
@@ -21,14 +22,13 @@ void Display::begin() {
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Hello, World!");
 }
 
 void Display::renderMenu(int selectedIndex) {
   // state_ = ScreenState::MENU;
 
   // should i guard the render methods to make sure lcd is present
-  lcd.clear();
+  // lcd.clear();
   lcd.setCursor(0, 0);
   if (selectedIndex == 0) {
     lcd.print(">");
@@ -46,50 +46,55 @@ void Display::renderMenu(int selectedIndex) {
   lcd.print("Config");
 }
 
-// void Display::renderConfig(int selectedIndex) {
-//   state_ = ScreenState::CONFIG;
+void Display::renderConfig(int selectedIndex) {
+  // state_ = ScreenState::CONFIG;
 
-//   lcd.setCursor(0, 0);
-//   if (selectedIndex == 0) {
-//     lcd.print(">");
-//   } else {
-//     lcd.print(" ");
-//   }
-//   lcd.print("5/3");
+  lcd.setCursor(0, 0);
+  if (selectedIndex == 0) {
+    lcd.print(">");
+  } else {
+    lcd.print(" ");
+  }
+  lcd.print("5/3");
 
-//   lcd.setCursor(0, 1);
-//   if (selectedIndex == 1) {
-//     lcd.print(">");
-//   } else {
-//     lcd.print(" ");
-//   }
-//   lcd.print("10/5");
-// }
+  lcd.setCursor(0, 1);
+  if (selectedIndex == 1) {
+    lcd.print(">");
+  } else {
+    lcd.print(" ");
+  }
+  lcd.print("10/5");
+}
 
-// void Display::renderTimer(int minutes, int seconds, bool isFocused, bool isPaused) {
-//   state_ = ScreenState::TIMER;
+void Display::renderTimer(int minutes, int seconds, bool isFocused, bool isPaused) {
+  // state_ = ScreenState::TIMER;
 
-//   lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);
 
-//   if (isFocused) {
-//     lcd.print("Focus");
-//   } else {
-//     lcd.print("Break");
-//   }
+  if (isFocused) {
+    lcd.print("Focus");
+  } else {
+    lcd.print("Break");
+  }
 
-//   lcd.setCursor(0, 1);
-//   if (minutes < 10) lcd.print("0");
-//   lcd.print(minutes);
-//   lcd.print(":");
-//   if (seconds < 10) lcd.print("0");
-//   lcd.print(seconds);
-//   lcd.print(" ");
+  lcd.setCursor(0, 1);
+  if (minutes < 10) lcd.print("0");
+  lcd.print(minutes);
+  lcd.print(":");
+  if (seconds < 10) lcd.print("0");
+  lcd.print(seconds);
+  lcd.print(" ");
 
 
-//   lcd.setCursor(5, 1);
-//   if (isPaused) {
-//     lcd.print(" PAUSED");
-//   } else {
-//     lcd.print("       ");
-//   }
-// }
+  lcd.setCursor(5, 1);
+  if (isPaused) {
+    lcd.print(" PAUSED");
+  } else {
+    lcd.print("       ");
+  }
+}
+
+void Display::clear() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+}
--- display.h
+++ display.h
@@ -9,6 +9,6 @@ public:
   void renderConfig(int selectedIndex);
   void clear();
 
-  private:
-    LiquidCrystal lcd;
+private:
+  LiquidCrystal lcd;
 };
